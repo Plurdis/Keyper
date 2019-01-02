11e53a5811624e38d49b4c57ba75dc6af6ef5dba
Plurdis <uutak2000@naver.com>
2019-01-02 10:46:57 +0900

    주석 추가 및 UI 변경

---
+++ Data/KeyperDataManager.cs
@@ -0,0 +1,17 @@
+﻿using Keyper.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keyper.Data
+{
+    public static class KeyperDataManager
+    {
+        public static ManagedItem GetManagedItem()
+        {
+            return null;
+        }
+    }
+}
--- Items/ManagedItem.cs
+++ Items/ManagedItem.cs
@@ -9,8 +9,31 @@ namespace Keyper.Items
 {
     public class ManagedItem : INotifyPropertyChanged
     {
+        static Dictionary<string, ManagedItem> ManagedDictionary = new Dictionary<string, ManagedItem>();
+
+        static ManagedItem()
+        {
+        }
+
+        public static void ImportItem()
+        {
+
+        }
+
+        public ManagedItem()
+        {
+        }
+
+        /// <summary>
+        /// 고유 식별자 입니다. 초기 생성시 자동으로 생성됩니다.
+        /// </summary>
+        public string Identifier { get; }
+
         private DateTime _lastTime;
 
+        /// <summary>
+        /// 가장 최근에 빌린 시각을 나타냅니다.
+        /// </summary>
         public DateTime LastTime
         {
             get => _lastTime;
@@ -21,20 +44,26 @@ namespace Keyper.Items
             }
         }
 
-        private string _type;
+        private string _classification;
 
-        public string Type
+        /// <summary>
+        /// 구분을 위한 부분입니다. (예. 교실, 실습실 등)
+        /// </summary>
+        public string Classification
         {
-            get => _type;
+            get => _classification;
             set
             {
-                _type = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Type"));
+                _classification = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Classification"));
             }
         }
 
         private string _name;
 
+        /// <summary>
+        /// 관리되는 아이템의 이름입니다.
+        /// </summary>
         public string Name
         {
             get => _name;
@@ -47,6 +76,9 @@ namespace Keyper.Items
 
         private bool _isRental;
 
+        /// <summary>
+        /// 현재 키의 상태가 빌려져 있는지에 대한 여부입니다.
+        /// </summary>
         public bool IsRental
         {
             get => _isRental;
